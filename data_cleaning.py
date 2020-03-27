cefaafa03c38a18f7aa229e0df768c8ffb7ab8c8
Jack Tann <jackbustertann@icloud.com>
2020-03-27 15:22:06 +0000

    minor changes to data cleaner function

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 def data_cleaner(input_file, output_file):
@@ -10,8 +11,6 @@ def data_cleaner(input_file, output_file):
 
     # changing column names
     raw_data.rename(columns = {'ap_hi': 'blood_pressure_max', 'ap_lo': 'blood_pressure_min', 'cholesterol': 'cholesterol_level', 'gluc': 'glucose_level', 'smoke': 'smoking_status', 'alco': 'alcohol_status', 'active': 'activity_level', 'cardio': 'disease'}, inplace = True)
-    # converting height to float
-    raw_data['height'] = raw_data['height'].astype('float')
     # converting age from days to years
     raw_data['age'] = raw_data['age'] // 365
     # converting gender into a binary variable
@@ -22,16 +21,15 @@ def data_cleaner(input_file, output_file):
     # changing sign of negative blood pressure values
     raw_data['blood_pressure_min'] = raw_data['blood_pressure_min'].map(lambda x: -1 * x if x < 0 else x)
     raw_data['blood_pressure_max'] = raw_data['blood_pressure_max'].map(lambda x: -1 * x if x < 0 else x)
-    # calculating blood pressure difference
-    raw_data['blood_pressure_diff'] = raw_data['blood_pressure_max'] - raw_data['blood_pressure_min']
     # dropping rows with diastolic blood pressure > systolic blood pressure
-    raw_data = raw_data.loc[raw_data['blood_pressure_diff'] > 0]
+    raw_data = raw_data.loc[raw_data['blood_pressure_min'] < raw_data['blood_pressure_max']]
     # dropping rows with infeasible blood pressure values
     raw_data = raw_data.loc[(raw_data['blood_pressure_max'] > 70) & (raw_data['blood_pressure_max'] < 200)]
     raw_data = raw_data.loc[(raw_data['blood_pressure_min'] > 40) & (raw_data['blood_pressure_min'] < 160)]
 
     # calculating bmi
     raw_data['bmi'] = raw_data['weight'] / (raw_data['height'] / 100)**2
+    raw_data['bmi'] = raw_data['bmi'].astype('int')
     # dropping height and weight columns
     raw_data.drop(columns = ['height', 'weight'], inplace = True)
     # dropping rows with infeasible bmi values
